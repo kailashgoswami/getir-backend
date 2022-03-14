950a4a1a0aa1dc7e9cd07953f363bf3d7c6b1600
kailash goswami <kailash.goswami@gmail.com>
2022-03-14 17:28:51 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+// server/index.js
+const jsonServer = require('json-server')
+const app = jsonServer.create()
+const path = require('path')
+const router = jsonServer.router('db.json');
+const PORT = process.env.PORT || 5001;
+const middlewares = jsonServer.defaults();
+
+const task = require('./api/task');
+
+app.use(middlewares);
+app.use('/api', router);
+app.get('/api/tasks', task.findAll);
+//app.get('/api/task/:id', task.findById);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+  app._router.stack.forEach(function(r){
+  if (r.route && r.route.path){
+    console.log(r.route.path)
+  }
+})
+});
